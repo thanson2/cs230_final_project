625eae04ab1836065193a70f413472007821c386
tdogger <tahanson2@wisc.edu>
2024-11-11 19:40:37 -0600

    add files

---
+++ src/model.py
@@ -0,0 +1,361 @@
+import math
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from tensorflow.keras.models import Model
+from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras.layers import Input, Dense, Dropout, LayerNormalization, MultiHeadAttention, Add, GlobalAveragePooling1D
+import yfinance as yf
+
+def calculate_bollinger_bands(data, window=10, num_of_std=2):
+    """Calculate Bollinger Bands"""
+    rolling_mean = data.rolling(window=window).mean()
+    rolling_std = data.rolling(window=window).std()
+    upper_band = rolling_mean + (rolling_std * num_of_std)
+    lower_band = rolling_mean - (rolling_std * num_of_std)
+    return upper_band, lower_band
+
+def calculate_rsi(data, window=10):
+    """Calculate Relative Strength Index"""
+    delta = data.diff()
+    gain = delta.clip(lower=0)
+    loss = -delta.clip(upper=0)
+    avg_gain = gain.rolling(window=window, min_periods=1).mean()
+    avg_loss = loss.rolling(window=window, min_periods=1).mean()
+    rs = avg_gain / avg_loss
+    rsi = 100 - (100 / (1 + rs))
+    return rsi
+
+def calculate_roc(data, periods=10):
+    """Calculate Rate of Change."""
+    roc = ((data - data.shift(periods)) / data.shift(periods)) * 100
+    return roc
+
+tickers = ['^GSPC']
+
+# Load the CSV file, only reading the necessary columns
+additional_data = pd.read_csv('RawData.csv',
+                              parse_dates=['Date'],
+                              index_col='Date',
+                              usecols=['Date', 'Price to Earnings Ratio', 'Enterprise Value to Sales'])
+
+# Rename columns if necessary to avoid conflicts
+additional_data = additional_data.rename(columns={
+    'Price to Earnings Ratio': '^GSPC_PE_Ratio',
+    'Enterprise Value to Sales': '^GSPC_EV_to_Sales'
+})
+
+ticker_data_frames = []
+stats = {}
+for ticker in tickers:
+
+    # Download historical data for the ticker
+    # data = yf.download(ticker, period="10y", interval="1d")
+    data = yf.download(ticker, start="2009-10-16", end = datetime.today().strftime('%Y-%m-%d'), interval="1d")
+
+    # Calculate the daily percentage change
+    close = data['Close']
+    upper, lower = calculate_bollinger_bands(close, window=14, num_of_std=2)
+    width = upper - lower
+    rsi = calculate_rsi(close, window=14)
+    roc = calculate_roc(close, periods=14)
+    volume = data['Volume']
+    diff = data['Close'].diff(1)
+    percent_change_close = data['Close'].pct_change() * 100
+
+    # Create a DataFrame for the current ticker and append it to the list
+    ticker_df = pd.DataFrame({
+        ticker+'_close': close,
+        ticker+'_width': width,
+        ticker+'_rsi': rsi,
+        ticker+'_roc': roc,
+        ticker+'_volume': volume,
+        ticker+'_diff': diff,
+        ticker+'_percent_change_close': percent_change_close,
+    })
+
+    # Merge with additional_data
+    ticker_df = pd.merge(ticker_df, additional_data, left_index=True, right_index=True, how='left')
+
+    # Calculate mean and std for all columns
+    MEAN = ticker_df.mean()
+    STD = ticker_df.std()
+
+    # Keep track of mean and std
+    for column in MEAN.index:
+        stats[f"{column}_mean"] = MEAN[column]
+        stats[f"{column}_std"] = STD[column]
+
+    # Normalize the training features
+    ticker_df = (ticker_df - MEAN) / STD
+
+    ticker_data_frames.append(ticker_df)
+
+# Concatenate all ticker DataFrames
+df = pd.concat(ticker_data_frames, axis=1)
+
+# Replace infinities with NaN and drop NaN values
+df.replace([np.inf, -np.inf], np.nan, inplace=True)
+df.dropna(inplace=True)
+
+print("Shape of final DataFrame:", df.shape)
+print("\nColumns in final DataFrame:", df.columns.tolist())
+print("\nFirst few rows of final DataFrame:")
+print(df.head())
+
+# Shift the df data to create labels
+labels = df.shift(-1)
+
+# Drop the last row in both percent_change_data and labels as it won't have a corresponding label
+df = df.iloc[:-1]
+labels = labels.iloc[:-1]
+
+# Sequence len = 30 means that we have 30 days of data
+SEQUENCE_LEN = 30
+
+# Function to create X-day sequences for each ticker
+def create_sequences(data, labels, mean, std, sequence_length=SEQUENCE_LEN):
+    sequences = []
+    lab = []
+    data_size = len(data)
+
+    # + 1 because we want to predict the next day
+    for i in range(data_size - (sequence_length + 1)):
+      if i == 0:
+        continue
+
+      sequences.append(data[i:i + sequence_length])
+      lab.append([labels[i-1], labels[i + 1], mean[0], std[0]])
+
+    for i in range(0, len(lab)):
+      last_price_data = sequences[i][-1][0]
+      last_price_label = lab[i][0]
+
+      if not last_price_data == last_price_label:
+        print(f"ERROR : {last_price_data=} and {last_price_label=} are not equal")
+
+    return np.array(sequences), np.array(lab)
+
+
+sequences_dict = {}
+sequence_labels = {}
+for ticker in tickers:
+
+    # Extract close and volume data for the ticker
+    close = df[ticker+'_close'].values
+    width = df[ticker+'_width'].values
+    rsi = df[ticker+'_rsi'].values
+    roc = df[ticker+'_roc'].values
+    volume = df[ticker+'_volume'].values
+    diff = df[ticker+'_diff'].values
+    pct_change = df[ticker+'_percent_change_close'].values
+    pe = df[ticker+'_PE_Ratio'].values
+    ev_sales = df[ticker+'_EV_to_Sales'].values
+
+    # Combine close and volume data
+    ticker_data = np.column_stack((close,
+                                   width,
+                                   rsi,
+                                   roc,
+                                   volume,
+                                   diff,
+                                   pct_change,
+                                   pe,
+                                   ev_sales))
+
+    # Generate sequences
+    attribute = ticker+"_close"
+    ticker_sequences, lab = create_sequences(ticker_data,
+                                             labels[attribute].values[SEQUENCE_LEN-1:],
+                                             stats[attribute+"_mean"].values,
+                                             stats[attribute+"_std"].values)
+
+    sequences_dict[ticker] = ticker_sequences
+    sequence_labels[ticker] = lab
+
+print(f"Shape of ticker_data for {ticker}: {ticker_data.shape}")
+
+# Combine data and labels from all tickers
+all_sequences = []
+all_labels = []
+
+for ticker in tickers:
+    all_sequences.extend(sequences_dict[ticker])
+    all_labels.extend(sequence_labels[ticker])
+
+# Convert to numpy arrays
+all_sequences = np.array(all_sequences)
+all_labels = np.array(all_labels)
+
+np.random.seed(42)
+shuffled_indices = np.random.permutation(len(all_sequences))
+all_sequences = all_sequences[shuffled_indices]
+all_labels = all_labels[shuffled_indices]
+
+train_size = int(len(all_sequences) * 0.9)
+
+# Split sequences
+train_sequences = all_sequences[:train_size]
+train_labels    = all_labels[:train_size]
+
+other_sequences = all_sequences[train_size:]
+other_labels    = all_labels[train_size:]
+
+shuffled_indices = np.random.permutation(len(other_sequences))
+other_sequences = other_sequences[shuffled_indices]
+other_labels = other_labels[shuffled_indices]
+
+val_size = int(len(other_sequences) * 0.5)
+
+validation_sequences = other_sequences[:val_size]
+validation_labels = other_labels[:val_size]
+
+test_sequences = other_sequences[val_size:]
+test_labels = other_labels[val_size:]
+
+def transformer_encoder(inputs, head_size, num_heads, ff_dim, dropout=0):
+    # Attention and Normalization
+    x = LayerNormalization(epsilon=1e-6)(inputs)
+    x = MultiHeadAttention(key_dim=head_size, num_heads=num_heads, dropout=dropout)(x, x)
+    x = Add()([x, inputs])
+
+    # Feed Forward Part
+    y = LayerNormalization(epsilon=1e-6)(x)
+    y = Dense(ff_dim, activation="relu")(y)
+    y = Dropout(dropout)(y)
+    y = Dense(inputs.shape[-1])(y)
+    return Add()([y, x])
+
+def build_transformer_model(input_shape, head_size, num_heads, ff_dim, num_layers, dropout=0):
+    inputs = Input(shape=input_shape)
+    x = inputs
+
+    # Create multiple layers of the Transformer block
+    for _ in range(num_layers):
+        x = transformer_encoder(x, head_size, num_heads, ff_dim, dropout)
+
+    # Final part of the model
+    x = GlobalAveragePooling1D()(x)
+    x = LayerNormalization(epsilon=1e-6)(x)
+    outputs = Dense(1, activation="linear")(x)
+
+    # Compile model
+    model = Model(inputs=inputs, outputs=outputs)
+    return model
+
+# Model parameters
+input_shape = train_sequences.shape[1:]
+head_size = 256
+num_heads = 16
+ff_dim = 1024
+num_layers = 12
+dropout = 0.20
+
+# Build the model
+model = build_transformer_model(input_shape, head_size, num_heads, ff_dim, num_layers, dropout)
+model.summary()
+
+def custom_mae_loss(y_true, y_pred):
+    y_true_next = tf.cast(y_true[:, 1], tf.float64)
+    y_pred_next = tf.cast(y_pred[:, 0], tf.float64)
+    abs_error = tf.abs(y_true_next - y_pred_next)
+
+    return tf.reduce_mean(abs_error)
+
+def dir_acc(y_true, y_pred):
+    mean, std = tf.cast(y_true[:, 2], tf.float64), tf.cast(y_true[:, 3], tf.float64)
+
+    y_true_prev = (tf.cast(y_true[:, 0], tf.float64) * std) + mean
+    y_true_next = (tf.cast(y_true[:, 1], tf.float64) * std) + mean
+    y_pred_next = (tf.cast(y_pred[:, 0], tf.float64) * std) + mean
+
+    true_change = y_true_next - y_true_prev
+    pred_change = y_pred_next - y_true_prev
+
+    correct_direction = tf.equal(tf.sign(true_change), tf.sign(pred_change))
+
+    return tf.reduce_mean(tf.cast(correct_direction, tf.float64))
+
+# Compile the model
+optimizer = tf.keras.optimizers.Adam()
+model.compile(optimizer=optimizer, loss=custom_mae_loss, metrics=[dir_acc])
+
+# Define a callback to save the best model
+checkpoint_callback_train = ModelCheckpoint(
+    "transformer_train_model.keras",  # Filepath to save the best model
+    monitor="dir_acc",  #"loss",  # Metric to monitor
+    save_best_only=True,  # Save only the best model
+    mode="max",  # Minimize the monitored metric
+    verbose=1,  # Display progress
+)
+
+# Define a callback to save the best model
+checkpoint_callback_val = ModelCheckpoint(
+    "transformer_val_model.keras",  # Filepath to save the best model
+    monitor="val_dir_acc", #"val_loss",  # Metric to monitor
+    save_best_only=True,  # Save only the best model
+    mode="max",  # Minimize the monitored metric
+    verbose=1,  # Display progress
+)
+
+def get_lr_callback(batch_size=16, mode='cos', epochs=500, plot=False):
+    lr_start, lr_max, lr_min = 0.0001, 0.005, 0.00001  # Adjust learning rate boundaries
+    lr_ramp_ep = int(0.30 * epochs)  # 30% of epochs for warm-up
+    lr_sus_ep = max(0, int(0.10 * epochs) - lr_ramp_ep)  # Optional sustain phase, adjust as needed
+
+    def lrfn(epoch):
+        if epoch < lr_ramp_ep:  # Warm-up phase
+            lr = (lr_max - lr_start) / lr_ramp_ep * epoch + lr_start
+        elif epoch < lr_ramp_ep + lr_sus_ep:  # Sustain phase at max learning rate
+            lr = lr_max
+        elif mode == 'cos':
+            decay_total_epochs, decay_epoch_index = epochs - lr_ramp_ep - lr_sus_ep, epoch - lr_ramp_ep - lr_sus_ep
+            phase = math.pi * decay_epoch_index / decay_total_epochs
+            lr = (lr_max - lr_min) * 0.5 * (1 + math.cos(phase)) + lr_min
+        else:
+            lr = lr_min  # Default to minimum learning rate if mode is not recognized
+
+        return lr
+
+    if plot:  # Plot learning rate curve if plot is True
+        plt.figure(figsize=(10, 5))
+        plt.plot(np.arange(epochs), [lrfn(epoch) for epoch in np.arange(epochs)], marker='o')
+        plt.xlabel('Epoch')
+        plt.ylabel('Learning Rate')
+        plt.title('Learning Rate Scheduler')
+        plt.show()
+
+    return tf.keras.callbacks.LearningRateScheduler(lrfn, verbose=True)
+
+BATCH_SIZE = 64
+EPOCHS = 100
+model.fit(train_sequences, train_labels,
+          validation_data=(validation_sequences, validation_labels),
+          epochs=EPOCHS,
+          batch_size=BATCH_SIZE,
+          shuffle=True,
+          callbacks=[checkpoint_callback_train, checkpoint_callback_val, get_lr_callback(batch_size=BATCH_SIZE, epochs=EPOCHS)])
+
+# Load Weights
+model.load_weights("transformer_val_model.keras")
+
+# Make predictions
+accuracy = model.evaluate(test_sequences, test_labels)[1]
+print(accuracy)
+
+# Calculate additional metrics as needed
+from sklearn.metrics import r2_score
+
+predictions = model.predict(test_sequences)
+r2 = r2_score(test_labels[:, 1], predictions[:, 0])
+print(f"R-squared: {r2}")
+
+end_time = time.time()
+
+elapsed_time = end_time - start_time
+
+# Convert elapsed time to hours, minutes, and seconds
+hours, rem = divmod(elapsed_time, 3600)
+minutes, seconds = divmod(rem, 60)
+
+print(f"Total runtime: {int(hours):02}:{int(minutes):02}:{int(seconds):02}")
